006d7a9ed5061ca4dacd235a11eeb5cdd9c9afac
oliviarossi <oliviarossi@pm.me>
2020-10-10 16:38:56 -0400

    removed vehicle file and projects

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import Hero from './Hero';
-// import Projects from './Projects/Projects';
 //import Contact from './Contact';
 import Footer from './Footer';
 
@@ -8,15 +7,12 @@ import { PortfolioProvider } from './context';
 
 function App() {
   const [hero, setHero] = useState({});
-  // const [projects, setProjects] = useState([]);
   //const [contact, setContact] = useState({});
   const [footer, setFooter] = useState({});
 
-  // add about, projects to portfolio provider
   return (
     <PortfolioProvider value={{ hero, footer }}>
       <Hero />
-      {/* <Projects /> */}
       {/* <Contact /> */}
       <Footer />
     </PortfolioProvider>
